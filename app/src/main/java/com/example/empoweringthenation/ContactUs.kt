7ec38bf75cfab04da2e38ab38a45e0303c121835
Nhlanhla Cindi <ST10470261@vcconnect.edu.za>
2025-08-22 09:40:42 +0200

    first commit

---
+++ app/src/main/java/com/example/empoweringthenation/ContactUs.kt
@@ -0,0 +1,34 @@
+package com.example.empoweringthenation
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import kotlin.system.exitProcess
+
+class ContactUs : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_contact_us)
+        val homeBtn = findViewById<Button>(R.id.homeBtn)
+        val exitBtn = findViewById<Button>(R.id.exitBtn)
+
+        homeBtn.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish() // Finish this activity to remove it from the back stack
+        }
+
+        // Set up Exit App button click listener
+        exitBtn.setOnClickListener {
+            finishAffinity() // Closes all activities in the task associated with this activity
+            exitProcess(0)
+        }
+    }
+}
---
+++ app/src/main/java/com/example/empoweringthenation/Login.kt
@@ -0,0 +1,62 @@
+package com.example.empoweringthenation
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class Login : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_login)
+
+        val email = findViewById<EditText>(R.id.emailTxt)
+        val password = findViewById<EditText>(R.id.passwordTxt)
+        val loginBtn = findViewById<Button>(R.id.btnLogin)
+        val signUpBtn = findViewById<Button>(R.id.btnSignUp)
+
+        val sharedPref = getSharedPreferences("UserPrefs", Context.MODE_PRIVATE)
+
+// LOGIN button
+        loginBtn.setOnClickListener {
+            val savedEmail = sharedPref.getString("email", null)
+            val savedPassword = sharedPref.getString("password", null)
+
+            if (email.text.toString() == savedEmail && password.text.toString() == savedPassword) {
+                Toast.makeText(this, "Login successful", Toast.LENGTH_SHORT).show()
+                startActivity(Intent(this, CourseDetails::class.java))
+
+            } else {
+                Toast.makeText(this, "Account not found. Please sign up.", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        // SIGN UP button
+        signUpBtn.setOnClickListener {
+            val emailText = email.text.toString()
+            val passwordText = password.text.toString()
+
+            if (emailText.isNotEmpty() && passwordText.isNotEmpty()) {
+                // Save new credentials
+                with(sharedPref.edit()) {
+                    putString("email", emailText)
+                    putString("password", passwordText)
+                    apply()
+                }
+
+                Toast.makeText(this, "Account created", Toast.LENGTH_SHORT).show()
+                startActivity(Intent(this, MainActivity::class.java))
+                finish()
+            } else {
+                Toast.makeText(this, "Please enter Email & Password", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+}
